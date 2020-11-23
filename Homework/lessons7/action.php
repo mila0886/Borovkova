003e6b51fca56f0c99484c6ab7d801cf134ae0a3
Liudmila <_mila_s@ukr.net>
2020-11-23 15:54:16 +0200

    commit

---
+++ Homework/lessons7/action.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+
+        <title>Сообщение благодарности</title>
+
+    </head>
+
+    <body>
+    
+        <div>Спасибо, с Вами свяжутся</div>
+    </body>
+</html>
